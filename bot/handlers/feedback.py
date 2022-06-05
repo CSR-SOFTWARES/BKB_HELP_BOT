24c9bc9f29118c7de7101ee2d5db63d3093fa3d3
CSR SOFTWARES <90786270+CSR-SOFTWARES@users.noreply.github.com>
2022-06-05 13:05:18 +0530

    Add files via upload

---
+++ bot/handlers/feedback.py
@@ -0,0 +1,110 @@
+import logging
+
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, Filters
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+import bot.configs as vars
+
+logging.getLogger(__name__).setLevel(logging.INFO)
+
+
+# IMPORTANT VARIABLES
+OWNER_ID = vars.OWNER_ID
+
+LOG_TEXT = "ID: <code>{}</code>\nName: <a href='tg://user?id={}'>{}{}</a>\nStarted the bot..."
+START_TEXT = "You Can Give Feedback and Contact Admins by Sending Messages to Me..."
+MESSAGE = "<b>Message from:</b> <code>{}</code>\n<b>Name:</b> <a href='tg://user?id={}'>{}{}</a>\n\n{}"
+
+
+# ADDING HANDLERS
+def add_feedback_handlers(bot):
+    bot.add_handler(
+        CommandHandler(command="start", callback=start, filters=Filters.chat_type.private, run_async=True)
+    )
+
+    bot.add_handler(
+        CommandHandler(command="about", callback=about, filters=Filters.chat_type.private, run_async=True)
+    )
+
+    bot.add_handler(
+        CallbackQueryHandler(pattern="about", callback=about, run_async=True)
+    )
+
+    bot.add_handler(
+        MessageHandler(filters=Filters.chat(OWNER_ID), callback=reply, run_async=True)
+    )
+
+    bot.add_handler(
+        MessageHandler(filters=Filters.chat_type.private, callback=user, run_async=True)
+    )
+
+
+
+
+
+#***************HANDLERS BELOW******************
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id = OWNER_ID,
+        text = LOG_TEXT.format(update.message.chat.id,update.message.chat.id,update.message.chat.first_name,"" if update.message.chat.last_name == None else " "+update.message.chat.last_name),
+        parse_mode = "html"
+    )
+    inline_keyboard = [[InlineKeyboardButton("💬GROUP💬", url = f"{vars.GROUP_LINK}"), InlineKeyboardButton("📢CHANNEL📢", url = f"{vars.CHANNEL_LINK}")], [InlineKeyboardButton("❗ABOUT❗", callback_data="about")]]
+    update.message.reply_text(
+        "*Hi {}!*\n".format(update.message.chat.first_name)+START_TEXT,
+        reply_markup = InlineKeyboardMarkup(inline_keyboard),
+        parse_mode = "markdown"
+    )
+
+def about(update, context):
+    bot_details = context.bot.get_me()
+    if update.message is not None:
+        message = update.message
+    else:
+        message = update.callback_query.message
+    message.reply_text(
+        "*My Name: [{}](tg://user?id={})\nSource: [Feedback\-and\-Request\-Bot\-TG](https://github.com/skr1405/Feedback\-and\-Request\-Bot\-TG)*".format(bot_details.first_name, bot_details.id),
+        parse_mode = "markdownv2",
+        disable_web_page_preview = True
+    )
+
+def reply(update, context):
+    if update.message.reply_to_message is not None:
+        replied_to = update.message.reply_to_message
+        try:
+            reference_id = replied_to.text.split()[2]
+        except Exception:
+            reference_id = replied_to.caption.split()[2]
+
+        if update.message.text != None:
+            context.bot.send_message(
+                text=update.message.text_html,
+                chat_id=int(reference_id),
+                parse_mode = "html"
+            )
+        else:
+            update.message.copy(
+                caption=update.message.caption_html,
+                chat_id=int(reference_id),
+                parse_mode = "html"
+            )
+        update.message.reply_text(
+            text = "Message Sent...✅",
+            quote = True
+        )
+
+def user(update, context):
+    info = update.message.from_user
+    reference_id = info.id
+    if update.message.text != None:
+        context.bot.send_message(
+            chat_id = OWNER_ID,
+            text = MESSAGE.format(reference_id, reference_id, info.first_name, "" if info.last_name == None else " "+info.last_name, update.message.text_html),
+            parse_mode = "html"
+        )
+    else:
+        update.message.copy(
+            chat_id = OWNER_ID,
+            caption = MESSAGE.format(reference_id, reference_id, info.first_name, "" if info.last_name == None else " "+info.last_name, update.message.caption_html if update.message.caption_html is not None else ""),
+            parse_mode = "html"
+        )
